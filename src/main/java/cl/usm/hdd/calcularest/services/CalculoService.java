097b78b77bed05f9b41c1cb3a8d472cc223e3db2
Seba Araya <arayariquelmes@gmail.com>
2023-08-25 16:39:37 -0400

    Agrega calculo basico

---
+++ src/main/java/cl/usm/hdd/calcularest/services/CalculoService.java
@@ -0,0 +1,6 @@
+package cl.usm.hdd.calcularest.services;
+
+public interface CalculoService {
+
+    double calcular(int num1, int num2, String operacion);
+}
---
+++ src/main/java/cl/usm/hdd/calcularest/services/CalculoServiceImpl.java
@@ -0,0 +1,18 @@
+package cl.usm.hdd.calcularest.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CalculoServiceImpl implements CalculoService {
+    @Override
+    public double calcular(int num1, int num2, String operacion) {
+        //TODO: Que pasa con el manejo de errores?
+        switch(operacion){
+            case "+": return num1 + num2;
+            case "-": return num1 - num2;
+            case "*": return num1*num2;
+            case "/": return (double)num1/(double)num2;
+            default: return 0;
+        }
+    }
+}
